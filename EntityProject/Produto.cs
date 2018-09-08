01225200daaf1feb17b0d400156809e128093979
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-08 19:43:04 -0400

    Removida classe ProdutoDAO

--- EntityProject/Produto.cs
+++ EntityProject/Produto.cs
@@ -11,11 +11,11 @@ namespace EntityProject
         public int Id { get; internal set; }
         public string Nome { get; internal set; }
         public string Categoria { get; internal set; }
-        public double Preco { get; internal set; }
+        public double PrecoUnitario { get; internal set; }
 
         public override string ToString()
         {
-            return $"Produto: {this.Id}, {this.Nome}, {this.Categoria}, {this.Preco}";
+            return $"Produto: {this.Id}, {this.Nome}, {this.Categoria}, {this.PrecoUnitario}";
         }
     }
 }
