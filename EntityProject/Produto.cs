644af0ebd4ff725f70a35ff4d63cfa2c09647be6
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-08 22:15:29 -0400

    Realizando migrações entre versões do banco de dados com Entity

--- EntityProject/Produto.cs
+++ EntityProject/Produto.cs
@@ -12,7 +12,7 @@ namespace EntityProject
         public string Nome { get; internal set; }
         public string Categoria { get; internal set; }
         public double PrecoUnitario { get; internal set; }
-        public String Unidade { get; internal set; }
+        public string Unidade { get; set; }
 
         public override string ToString()
         {
