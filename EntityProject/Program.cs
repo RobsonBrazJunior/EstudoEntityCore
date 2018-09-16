1954094868a6cd420dc1d13b6670a276d1e6157f
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-16 14:56:06 -0400

    Testando Promoção.

--- EntityProject/Program.cs
+++ EntityProject/Program.cs
@@ -14,13 +14,38 @@ namespace EntityProject
     {
         static void Main(string[] args)
         {
+            var p1 = new Produto()
+            {
+                Nome = "Suco de Laranja",
+                Categoria = "Bebidas",
+                PrecoUnitario = 8.76,
+                Unidade = "Litros"
+            };
+
+            var p2 = new Produto()
+            {
+                Nome = "Café",
+                Categoria = "Bebidas",
+                PrecoUnitario = 12.50,
+                Unidade = "Gramas"
+            };
+            
+            var p3 = new Produto()
+            {
+                Nome = "Macarrão",
+                Categoria = "Alimentos",
+                PrecoUnitario = 4.25,
+                Unidade = "Gramas"
+            };
+
             var promocaoDePascoa = new Promocao();
             promocaoDePascoa.Descricao = "Feliz Páscoa";
             promocaoDePascoa.DataInicio = DateTime.Now;
             promocaoDePascoa.DataTermino = DateTime.Now.AddMonths(3);
-            //promocaoDePascoa.Produtos.Add(new Produto());
-            //promocaoDePascoa.Produtos.Add(new Produto());
-            //promocaoDePascoa.Produtos.Add(new Produto());
+
+            promocaoDePascoa.IncluiProduto(p1);
+            promocaoDePascoa.IncluiProduto(p2);
+            promocaoDePascoa.IncluiProduto(p3);
 
 
             using (var contexto = new LojaContext())
@@ -29,7 +54,9 @@ namespace EntityProject
                 var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
                 loggerFactory.AddProvider(SqlLoggerProvider.Create());
 
-                
+                contexto.Promocoes.Add(promocaoDePascoa);
+                ExibeEntries(contexto.ChangeTracker.Entries());
+                contexto.SaveChanges();
             }
         }
 
